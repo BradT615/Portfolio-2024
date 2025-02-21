c6b68f41df24fc8a367e17d00ba527fab8750d16
BradT615 <125227539+BradT615@users.noreply.github.com>
2025-02-21 14:38:25 -0500

    og test generated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,18 +20,11 @@ export default function RootLayout({
         <meta name="description" content="Brad Titus - Full Stack Developer Portfolio" />
         <meta name="google-site-verification" content="1mlH0b6ZQUcucsv7tibARouXD4ATrO8zANWKFm103d0" />
 
-        <meta property="og:url" content="https://bradtitus.dev" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Brad Titus" />
-        <meta property="og:description" content="Brad Titus - Full Stack Developer Portfolio" />
-        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/e7b6b8e5-5bf9-4abd-86df-7327fc8851d7.png?token=5HEJjrkrqqz3AV7ZbHMYbllkZVmJTPnzeC2V9J5SpCY&height=630&width=1200&expires=33276165009" />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="bradtitus.dev" />
-        <meta property="twitter:url" content="https://bradtitus.dev" />
-        <meta name="twitter:title" content="Brad Titus" />
-        <meta name="twitter:description" content="Brad Titus - Full Stack Developer Portfolio" />
-        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/e7b6b8e5-5bf9-4abd-86df-7327fc8851d7.png?token=5HEJjrkrqqz3AV7ZbHMYbllkZVmJTPnzeC2V9J5SpCY&height=630&width=1200&expires=33276165009" />
 
         <link
           rel="stylesheet"
